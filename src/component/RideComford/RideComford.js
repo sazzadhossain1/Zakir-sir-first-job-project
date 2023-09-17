1972c7ebaf24f8846eb93f06fdd17c08f3743164
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-17 14:15:21 +0600

    create a ride in comfort cart and apply grid method

--- src/component/RideComford/RideComford.js
+++ src/component/RideComford/RideComford.js
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 
 const RideComford = () => {
   return (
-    <div>
-      <h1>Ride In Comfort</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-        rem autem?
+    <div className="rideComford-parent-div">
+      <h1 className="rideInComfort">Ride In Comfort</h1>
+      <p className="rodeComfort-p-first-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+        luctus nec ullamcorper <br /> mattis, pulvinar dapibus leo.
       </p>
-      <div>
-        <div>
-          <div>
-            <h3>Iteksi Regular</h3>
-            <p>
+      <div className="rideInComfort-grid-div">
+        <div className="">
+          <div className="rideInComfort-car-cart-first-text-div">
+            <h3 className="rideInComfort-heading">Iteksi Regular</h3>
+            <p className="rideInComfort-p">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatem, in ipsum!
             </p>
-            <button>
+            <button className="learn-more">
               <Link>Learn More</Link>
             </button>
           </div>
-          <div>
+          <div className="car-photo-div">
             <img
               className="car-photo"
               src="https://kitpro.site/iteksi/wp-content/uploads/sites/184/2023/08/image-of-a-blue-car-on-different-positions-2022-12-15-22-53-57-utc-1-1024x472.png"
