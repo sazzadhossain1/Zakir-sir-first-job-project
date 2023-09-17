9ed84e4dc4245bc6813e201af3c6c199d883bcc4
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-17 15:47:20 +0600

    cart responsiveness done

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer-parent-div">
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
+      <h1>This is Footer</h1>
       <h1>This is Footer</h1>
     </div>
   );
