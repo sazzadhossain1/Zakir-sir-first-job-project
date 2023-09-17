1d552ddb026a23370406207fba631a06c55a590b
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-18 04:26:41 +0600

    add responsiveness in footer section and currection some responsiveness in million section

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
   return (
     <div className="footer-parent-div">
       <div className="footer-grid-div">
-        <div className="footer-single-cart-div">
+        <div className="footer-single-cart-div footer-border">
           <img
             className="footer-first-logo"
             src="https://pathao.com/wp-content/uploads/2019/03/Pathao-logo-inverted.png"
             alt=""
           />
-          <p>
+          <p className="footer-p">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
@@ -29,31 +29,34 @@ const Footer = () => {
           </div>
         </div>
         {/*  */}
-        <div>
-          <h3>NAVIGATION</h3>
-          <p>Home</p>
-          <p>Page</p>
-          <p>About Us</p>
-          <p>Service</p>
-          <p>Help</p>
+        <div className="footer-single-cart-div footer-border">
+          <h3 className="footer-heading">NAVIGATION</h3>
+          <p className="footer-p">Home</p>
+          <p className="footer-p">Page</p>
+          <p className="footer-p">About Us</p>
+          <p className="footer-p">Service</p>
+          <p className="footer-p">Help</p>
         </div>
         {/*  */}
-        <div>
-          <h3>QUICK LINK</h3>
-          <p>Contact Us</p>
-          <p>FAQs</p>
-          <p>Blog</p>
-          <p>Gallery</p>
-          <p>Pricing</p>
+        <div className="footer-single-cart-div footer-border">
+          <h3 className="footer-heading">QUICK LINK</h3>
+          <p className="footer-p">Contact Us</p>
+          <p className="footer-p">FAQs</p>
+          <p className="footer-p">Blog</p>
+          <p className="footer-p">Gallery</p>
+          <p className="footer-p">Pricing</p>
         </div>
         {/*  */}
-        <div>
-          <h3>WORK HOURS</h3>
+        <div className="footer-single-cart-div footer-border-2">
+          <h3 className="footer-heading">WORK HOURS</h3>
           <p>
-            <i class="fa-regular fa-clock"></i> 7AM-5PM, Mon - Sat
+            <i class="fa-regular fa-clock footer-i"></i>
+            <span className="footer-p">7AM-5PM, Mon - Sat</span>
           </p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <Link>
+          <p className="footer-p">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <Link className="footer-callUs-button">
             <i class="fa-solid fa-phone"></i>
             <button>Call us</button>
           </Link>
