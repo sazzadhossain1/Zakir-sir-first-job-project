29b3b6d41893b69fb964de24873fc9f73802b8be
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-18 01:15:55 +0600

    add fontawsom  file and add some text in footer section with some style

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -4,24 +4,58 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-parent-div">
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
-      <h1>This is Footer</h1>
+      <div>
+        <div className="ourAchievement-div">
+          <h1 className="ourAchievement-heading">Our Achievement</h1>
+          <p className="ourAchievement-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+        </div>
+        <div className="footer-grid-div">
+          <div className="border-div">
+            <div>
+              <div>
+                <i class="fa-solid fa-car"></i>
+              </div>
+              <p>30K+</p>
+            </div>
+            <h3>Experience</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur <br />
+              adipisicing elit. Pariatur, ad!
+            </p>
+          </div>
+          <div className="border-div">
+            <div>
+              <div>
+                <i class="fa-solid fa-users"></i>
+              </div>
+              <p>300K+</p>
+            </div>
+            <h3>Experience</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur <br />
+              adipisicing elit. Pariatur, ad!
+            </p>
+          </div>
+          <div className="footer-cart-div">
+            <div>
+              <div>
+                <i class="fa-solid fa-car"></i>
+              </div>
+              <p>50K+</p>
+            </div>
+            <h3>Experience</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur <br />
+              adipisicing elit. Pariatur, ad!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
