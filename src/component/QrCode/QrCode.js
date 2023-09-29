951d1f94e1babc883a76c444be31bb3eed8f86b5
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-09-30 02:47:51 +0600

    create a new component and add qr code img in qrCode component with style

--- src/component/QrCode/QrCode.js
+++ src/component/QrCode/QrCode.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./QrCode.css";
+import qrCode from "../../images/Qr-code/qr-code.jpg";
 
 const QrCode = () => {
   return (
-    <div>
-      <h1>This is QR - Code</h1>
+    <div className="qr-code-parent-div">
+      <h1 className="qr-code-download-header">
+        Download Mobile App For Convenience
+      </h1>
+      <div className="qr-code-img-div">
+        <img className="qr-code-img" src={qrCode} alt="" />
+      </div>
     </div>
   );
 };
